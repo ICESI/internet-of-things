e2bc7e893976880f18ee814a638df457fc0d737d
Daniel Barragan <daniel.barragan@correounivalle.edu.co>
2016-03-11 00:49:54 +0000

    add first source code

---
+++ galileo_restful/01_restful_intro/iot_sensors.py
@@ -0,0 +1,87 @@
+from flask import Flask
+import logging
+
+#intel galileo libraries
+import mraa
+import time
+import random
+
+#prepare the gpio for output
+x_gpio = mraa.Gpio(8)
+x_gpio.dir(mraa.DIR_OUT)
+x_aio = mraa.Aio(0)
+
+print (mraa.getVersion())
+
+app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+#creating a file handler
+handler = logging.FileHandler('iot_sensors.log')
+handler.setLevel(logging.DEBUG)
+#creating a logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+#adding the handler to the logger
+logger.addHandler(handler)
+
+@app.route("/api/v1.0")
+@app.route("/api")
+@app.route("/")
+def api_version():
+	logger.info('Showing mraa version')
+	return mraa.getVersion()
+
+@app.route("/api/v1.0/iot_sensors/led")
+def set_led_status():
+	logger.info('Changing led status')
+	if x_gpio.read() == 1:
+		x_gpio.write(0)
+		logger.debug('led status: %d', x_gpio.read())
+		return "turned off"
+	else:
+		x_gpio.write(1)
+		logger.debug('led status: %d', x_gpio.read())
+		return "turned on"
+
+@app.route("/api/v1.0/iot_sensors/light")
+def get_intensity_light():
+	try:
+		print (x_aio.read())
+		print ("%.5f" % x_aio.readFloat())
+		logger.debug('the intensity of light is: %.5f', x_aio.readFloat())
+        	return str(x_aio.read())
+	except:
+		print ("Are you sure you have an ADC?")
+        	logger.debug('cannot read light intensity')
+	        return "error"
+
+@app.route("/api/v1.0/iot_sensors/virtual")
+def get_virtual_data():
+        logger.info('Reading virtual data')
+	sensor_value = random.random() 
+        logger.debug('button box status: %f', sensor_value)
+        return str(sensor_value) 
+
+@app.route("/api/v1.0/iot_sensors/<sensor_name>/<int:sensor_id>")
+def get_box(sensor_name,sensor_id):
+	logger.info('Selecting a sensor by type and id')
+	sensor_id = str(sensor_id)
+	return 'you selected a sensor of type ' + sensor_name + ' and id ' + sensor_id
+
+@app.route("/api/v1.0/iot_sensors/<sensor_name>/")
+def get_sensor_information(sensor_name):
+	logger.info('listing sensors')
+	return 'listing sensors of type: ' + sensor_name
+
+if __name__ == "__main__":
+	#allow to make changes in the source code and test
+	app.run('0.0.0.0',debug=True)
+
+
+# Review this
+#import flask
+#flask.__version__
+#http://flask.pocoo.org/docs/0.10/config/
